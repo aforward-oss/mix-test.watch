5e96201d66752eaf000e25f8e407d5c31d59190d
Andrew Forward <aforward@gmail.com>
2017-04-01 18:31:40 -0400

    Add support to configure :asni_enabled

--- lib/mix_test_watch/config.ex
+++ lib/mix_test_watch/config.ex
@@ -10,6 +10,7 @@ defmodule MixTestWatch.Config do
   @default_exclude []
   @default_extra_extensions []
   @default_cli_executable "mix"
+  @default_ansi_enabled true
 
 
   defstruct tasks:            @default_tasks,
@@ -19,6 +20,7 @@ defmodule MixTestWatch.Config do
             exclude:          @default_exclude,
             extra_extensions: @default_extra_extensions,
             cli_executable:   @default_cli_executable,
+            ansi_enabled:     @default_ansi_enabled,
             cli_args:         []
 
   @spec new([String.t]) :: %__MODULE__{}
@@ -33,6 +35,7 @@ defmodule MixTestWatch.Config do
       runner:            get_runner(),
       exclude:           get_excluded(),
       cli_executable:    get_cli_executable(),
+      ansi_enabled:      get_ansi_enabled(),
       cli_args:          cli_args,
       extra_extensions:  get_extra_extensions(),
     }
@@ -62,6 +65,11 @@ defmodule MixTestWatch.Config do
   defp get_cli_executable do
     Application.get_env(:mix_test_watch, :cli_executable,
                         @default_cli_executable)
+  end
+
+  defp get_ansi_enabled do
+    Application.get_env(:mix_test_watch, :ansi_enabled,
+                        @default_ansi_enabled)
   end
 
   defp get_extra_extensions do
--- test/mix_test_watch/port_runner/port_runner_test.exs
+++ test/mix_test_watch/port_runner/port_runner_test.exs
@@ -19,5 +19,19 @@ defmodule MixTestWatch.PortRunnerTest do
               <> "'Application.put_env(:elixir, :ansi_enabled, true);', test"
       assert PortRunner.build_tasks_cmds(config) == expected
     end
+
+    test "take the command ansi_enabled and handle :ignore" do
+      config = %Config{ ansi_enabled: :ignore }
+      expected = "MIX_ENV=test mix test"
+      assert PortRunner.build_tasks_cmds(config) == expected
+    end
+
+    test "take the command ansi_enabled and handle false" do
+      config = %Config{ ansi_enabled: false }
+      expected = "MIX_ENV=test mix do run -e "
+              <> "'Application.put_env(:elixir, :ansi_enabled, false);', test"
+      assert PortRunner.build_tasks_cmds(config) == expected
+    end
+
   end
 end
